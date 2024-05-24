1cdd7274abca989e79e0a63125d8c7fae5a18145
11211016@student.itk.ac.id <11211016@student.itk.ac.id>
2024-05-24 23:22:21 +0800

    test

--- react/src/components/editProduct/editProduct.jsx
+++ react/src/components/editProduct/editProduct.jsx
@@ -4,6 +4,9 @@ const editProduct = () => {
       <div>
         <h1>Ini adalah halaman Edit Product</h1>
       </div>
+      <div>
+        <h1>Ini adalah halaman Edit Product</h1>
+      </div>
     </>
   );
 };
