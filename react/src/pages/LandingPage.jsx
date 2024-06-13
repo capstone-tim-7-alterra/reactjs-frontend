93824f413805cbf2ae7157476fe33205b3f5d662
alifharesta <111202113768@mhs.dinus.ac.id>
2024-06-13 15:12:52 +0700

    fix landingPage

--- react/src/pages/LandingPage.jsx
+++ react/src/pages/LandingPage.jsx
@@ -1,9 +1,9 @@
-import HeroSection from "../components/landingPage/HeroSection";
+import HeroSection from "../components/landingPage/HeroSections";
 import Nav from "../components/landingPage/NavbarLP";
-import Benefit from "../components/landingPage/Benefit";
+import Benefit from "../components/landingPage/Benefits";
 import Appscreen from "../components/landingPage/Appscreen";
-import Testimoni from "../components/landingPage/Testimoni";
-import Footer from "../components/landingPage/Footer";
+import Testimoni from "../components/landingPage/Testimonis";
+import Footer from "../components/landingPage/Footers";
 export default function HomePage() {
     return (
         <div className='bg-base-170'>
