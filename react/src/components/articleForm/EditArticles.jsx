7d9b1fef960407d1a83113cef891d9eeaa42636e
Raisal Ahmad <raisal.ahmad@mail.ugm.ac.id>
2024-06-23 03:09:55 +0700

    Add: Implementation Form Validation Edit Articles (Done)

--- react/src/components/articleForm/EditArticles.jsx
+++ react/src/components/articleForm/EditArticles.jsx
@@ -15,6 +15,7 @@ export default function EditPost() {
       tags: '',
       image: null,
     });
+    const [errors, setErrors] = useState({});
     const [previewImage, setPreviewImage] = useState(null);
   
     const fetcher = url => axiosInstance.get(url).then(res => res.data.data);
@@ -24,7 +25,6 @@ export default function EditPost() {
       if (data) {
         setFormData({
           title: data.title,
-          author: data.author,
           content: data.content,
           tags: data.tags,
           image: data.image || null,
@@ -40,13 +40,37 @@ export default function EditPost() {
         const imageUrl = URL.createObjectURL(file);
         setPreviewImage(imageUrl);
         setFormData({ ...formData, image: file });
+        setErrors((prev) => ({ ...prev, image: '' }));
       } else {
         setFormData({ ...formData, [name]: value });
+        setErrors((prev) => ({ ...prev, [name]: '' })); 
       }
     };
   
     const handleSubmit = (e) => {
-      e.preventDefault();
+        e.preventDefault();
+
+        const newErrors = {};
+
+        if (!formData.title?.trim()) {
+            newErrors.title = 'Post Title cannot be empty';
+        }
+        if (!formData.content?.trim()) {
+            newErrors.content = 'Post Content cannot be empty';
+        }
+        if (!formData.tags?.trim()) {
+            newErrors.tags = 'Tags cannot be empty';
+        }
+
+        if (!formData.image) {
+            newErrors.image = 'Post Image cannot be empty';
+        }
+
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
       const updatedData = new FormData();
       updatedData.append('title', formData.title);
       updatedData.append('content', formData.content);
@@ -61,15 +85,12 @@ export default function EditPost() {
           console.error('Error updating article:', err);
         });
     };
-  
-    if (!data) return <p>Loading...</p>;
-    if (error) return <p>Error loading article</p>;
 
     return (
         <>
             <section className="container xl:px-[45px] xl:h-[1560px]">
                 <Breadcrumbs />
-                <form className="form-secondary" onSubmit={handleSubmit}>
+                <form className="form-secondary" onSubmit={handleSubmit} noValidate>
                     <h1 className="title-form">
                         Edit Post
                     </h1>
@@ -77,26 +98,31 @@ export default function EditPost() {
                     <article className="question-form">
                         <div className="question-card">
                             <h2 className="text-zinc-900 lg:text-base lg:font-semibold leading-tight">
-                                Post Title *
+                                Post Title <span className={`${errors.title ? 'text-red-500' : ''}`} >*</span>
                             </h2>
                             <textarea 
                                 name="title"
                                 placeholder="Title" 
-                                className="xl:w-[1115px] xl:h-12 textarea textarea-bordered textarea-sm w-full bg-primary-100" 
+                                className={`xl:w-[1115px] xl:h-12 textarea textarea-bordered textarea-sm w-full bg-primary-100 ${errors.title ? 'border-red-500' : ''}`}
                                 value={formData.title}
                                 onChange={handleChange}
-                                required
                             ></textarea>
-                            <div className="text-zinc-900 text-sm font-normal leading-tight text-justify">
-                                This will be shown as the title on the front page and on the post itself
-                            </div>
+                            {errors.title ? (
+                                <div className="text-red-500 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    {errors.title}
+                                </div>
+                            ) : (
+                                <div className="text-zinc-900 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    This will be shown as the title on the front page and on the post itself
+                                </div>
+                            )}
                         </div>
 
                         <div className="question-card">
                             <h2 className="text-zinc-900 lg:text-base lg:font-semibold leading-tight">
-                                Post Image
+                                Post Image <span className={`${errors.image ? 'text-red-500' : ''}`} >*</span>
                             </h2>
-                            <div className="media-input">
+                            <div className={`media-input ${errors.image ? 'border-red-500' : ''}`}>
                                 <label htmlFor="file-upload" className="cursor-pointer gap-2 mx-auto">
                                 {previewImage ? (
                                     <img
@@ -125,39 +151,55 @@ export default function EditPost() {
                                     onChange={handleChange}
                                 />
                             </div>
-                            <div className="text-zinc-900 text-sm font-normal leading-tight text-justify">
-                                This image will appear in the header of the post when it is viewed. The recommended size is 500x500px
-                            </div>
+                            {errors.image ? (
+                                <div className="text-red-500 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    {errors.image}
+                                </div>
+                            ) : (
+                                <div className="text-zinc-900 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    This image will appear in the header of the post when it is viewed. The recommended size is 500x500px
+                                </div>
+                            )}
                         </div>
 
                         <div className="question-card">
                             <h2 className="text-zinc-900 lg:text-base lg:font-semibold leading-tight">
-                                Post Content *
+                                Post Content <span className={`${errors.content ? 'text-red-500' : ''}`} >*</span>
                             </h2>
                             <textarea 
                                 name="content"
                                 value={formData.content}
                                 onChange={handleChange}
                                 placeholder="Description" 
-                                className="xl:w-[1115px] xl:h-[229px] textarea textarea-bordered textarea-sm w-full bg-primary-100"
-                                required
+                                className={`xl:w-[1115px] xl:h-[229px] textarea textarea-bordered textarea-sm w-full bg-primary-100 ${errors.content ? 'border-red-500' : ''}`}
                             ></textarea>
-                            <div className="text-zinc-900 text-sm font-normal leading-tight text-justify">
-                                This is your post content
-                            </div>
+                            {errors.content ? (
+                                <div className="text-red-500 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    {errors.content}
+                                </div>
+                            ) : (
+                                <div className="text-zinc-900 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    This is your post content
+                                </div>
+                            )}
                         </div>
 
                         <div className="question-card">
                             <h2 className="text-zinc-900 lg:text-base lg:font-semibold leading-tight">
-                                Tags
+                                Tags <span className={`${errors.tags ? 'text-red-500' : ''}`} >*</span>
                             </h2>
-                            <textarea 
+                            <input 
                                 name="tags"
                                 value={formData.tags}
                                 onChange={handleChange}
                                 placeholder="Tags" 
-                                className="xl:w-[1115px] xl:h-12 textarea textarea-bordered textarea-sm w-full bg-primary-100" 
-                            ></textarea>
+                                className={`xl:w-[1115px] xl:h-12 input input-bordered input-sm w-full bg-primary-100 ${errors.tags ? 'border-red-500' : ''}`}
+                            ></input>
+                            {errors.tags && (
+                                <div className="text-red-500 text-xs md:text-sm font-normal leading-tight text-justify">
+                                    {errors.tags}
+                                </div>
+                            )}
                         </div>
 
                         <div className="w-full inline-flex sm:gap-3 xl:gap-[21px] sm:justify-center md:justify-end">
